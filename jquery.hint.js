32be351b072cd4833e263f6b6342cddd6960637c
Robin Mehner <robin@coding-robin.de>
2009-10-20 22:37:40 +0200

    Modified jquery.hint.js to use an optional hint text param, leave it blank if you want to use the title attribute instead

---
+++ jquery.hint.js
@@ -0,0 +1,46 @@
+/**
+* @author Remy Sharp
+* @url http://remysharp.com/2007/01/25/jquery-tutorial-text-box-hints/
+* 
+* modified to use the optional hint text if you don't want to rely on the
+* title attribute
+*/
+
+(function ($) {
+
+$.fn.hint = function (hintText, blurClass) {
+  if (!blurClass) { 
+    blurClass = 'blur';
+  }
+    
+  return this.each(function () {
+    // get jQuery version of 'this'
+    var $input = $(this),
+      // capture the rest of the variable to allow for reuse
+      title = (!hintText) ? $input.attr('title') : hintText,
+      $form = $(this.form),
+      $win  = $(window);
+
+    function remove() {
+      if ($input.val() === title && $input.hasClass(blurClass)) {
+        $input.val('').removeClass(blurClass);
+      }
+    }
+
+    // only apply logic if the element has the attribute
+    if (title) { 
+      // on blur, set value to title attr if text is blank
+      $input.blur(function () {
+        if (this.value === '') {
+          $input.val(title).addClass(blurClass);
+        }
+      }).focus(remove).blur(); // now change all inputs to title
+      
+      // clear the pre-defined text when form is submitted
+      $form.submit(remove);
+      $win.unload(remove); // handles Firefox's autocomplete
+    }
+  });
+};
+
+})(jQuery);
